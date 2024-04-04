957b626e7c1a55c06c8c4b2f40a86c11eb7939c3
toxikdnb <toxikdnb@hotmail.com>
2024-04-04 20:07:20 +0100

    Implemented ColorManager

--- src/main/java/com/toxikdnb/ResourceManagement/ColorManager.java
+++ src/main/java/com/toxikdnb/ResourceManagement/ColorManager.java
@@ -4,5 +4,11 @@ package com.toxikdnb.ResourceManagement;
  * Manages the colors used in the application.
  */
 public class ColorManager {
+    // Colours: 2a2b2a 5e4955 996888 c99da3 c6ddf0
 
+    public static final String TEXT_COLOR = "#2a2b2a";
+    public static final String BUTTON_COLOR = "#5e4955";
+    public static final String MID_COLOUR = "#c99da3";
+    public static final String TEXT_COLOR_LIGHT = "#c6ddf0";
+    public static final String ACCENT_COLOR = "#996888";
 }
