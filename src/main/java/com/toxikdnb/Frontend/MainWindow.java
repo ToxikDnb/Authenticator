df2a7a7c073df2ad9b22102b3afba9837752960b
toxikdnb <toxikdnb@hotmail.com>
2024-04-04 20:34:18 +0100

    Implemented Adding Hashbuttons

--- src/main/java/com/toxikdnb/Frontend/MainWindow.java
+++ src/main/java/com/toxikdnb/Frontend/MainWindow.java
@@ -8,6 +8,9 @@ import com.toxikdnb.ResourceManagement.ColorManager;
 import com.toxikdnb.ResourceManagement.FontManager;
 
 public class MainWindow extends Window {
+
+    JPanel buttonPanel;
+
     public MainWindow(String name, int width, int height, boolean isResizable, Image icon) {
         super(name, width, height, isResizable, icon);
 
@@ -21,6 +24,22 @@ public class MainWindow extends Window {
         titleLabel.setBorder(new MatteBorder(0, 0, 2, 0, Color.darkGray));
         panel.add(titleLabel, BorderLayout.NORTH);
 
+        buttonPanel = new JPanel(new GridLayout(0, 1));
+        JScrollPane scrollPane = new JScrollPane(buttonPanel);
+        scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setBorder(null);
+        for (int i = 0; i < 20; i++) {
+            AddHashButton("Button " + i);
+        }
+        panel.add(scrollPane, BorderLayout.CENTER);
+
+        mainFrame.revalidate();
+    }
+
+    public void AddHashButton(String title) {
+        HashButton button = new HashButton(title);
+        buttonPanel.add(button);
         mainFrame.revalidate();
     }
 }
